b28ca8ba55e4f6a542c2fba921204dad502b78e4
Walter White <Walter White>
2017-04-27 22:55:47 -0700

    Versión 1

---
+++ sintaxis pila/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+
+            Stack Pila = new Stack();
+
+            Pila.Push(Console.ReadLine());
+
+            Pila.Push(Console.ReadLine());
+            Pila.Push(Console.ReadLine());
+            Pila.Push(Console.ReadLine());
+            Pila.Push(Console.ReadLine());
+
+            Array arreglo = Pila.ToArray();
+
+            foreach (string valor in arreglo)
+            {
+                Console.WriteLine(valor);
+            }
+
+            Console.ReadKey();
+
+            Pila.Pop();
+
+            Console.ReadKey();
+
+            Pila.Pop();
+
+            Console.ReadKey();
+
+            Pila.Pop();
+
+            Console.ReadKey();
+
+            Pila.Pop();
+
+            Console.ReadKey();
+
+            //elimina el último valor introducido
+            Pila.Pop();
+
+
+            //Muestra el ´8ltimo valor introducido
+            Pila.Peek();
+
+        }
+
+       
+
+
+
+
+    }
+}
